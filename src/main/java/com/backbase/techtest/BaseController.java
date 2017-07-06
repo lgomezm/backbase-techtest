203496497a11d630370940eee46d5c93f8245758
Gomez, Luis <lgomez@cvent.com>
2017-07-06 18:28:34 -0500

    Basic rest service added

--- src/main/java/com/backbase/techtest/BaseController.java
+++ src/main/java/com/backbase/techtest/BaseController.java
@@ -1,26 +1,27 @@
 package com.backbase.techtest;
 
+import java.util.Date;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class BaseController {
 
-	private static int counter = 0;
-	private static final String VIEW_INDEX = "index";
 	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
-
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String welcomeName(@PathVariable String name, ModelMap model) {
-
-		model.addAttribute("message", "Welcome " + name);
-		model.addAttribute("counter", ++counter);
-		logger.debug("[welcomeName] counter : {}", counter);
-		return VIEW_INDEX;
-
+	
+	@RequestMapping(value = "/person", method = RequestMethod.GET)
+	public @ResponseBody Person getDummyEmployee() {
+		logger.info("Start getDummyEmployee");
+		Person emp = new Person();
+		emp.setAge(26);
+		emp.setName("Dummy");
+		return emp;
 	}
 }
